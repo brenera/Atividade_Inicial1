e4607c6f6df989ff04cd314d40a1b6cdc221c9f2
Breno Henrique <50157380+brenera@users.noreply.github.com>
2020-02-13 20:34:07 -0300

    Initial commit

---
+++ Atividade_Inicial/Calcular.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade_Inicial
+{
+     public class Calcular
+    {
+        public double valorInicial;
+        public int qtdParcelas;
+
+        public double CalcularParcelas()
+        {
+            double resultado = valorInicial / qtdParcelas;
+
+            if(qtdParcelas>= 5)
+            {
+                resultado = resultado + resultado * 1 / 100;
+            }
+
+            return (resultado);
+        }
+       
+    }
+}
---
+++ Atividade_Inicial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Atividade_Inicial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calcular calcular = new Calcular();
+
+            Console.Write("Valor Inicial: ");
+            calcular.valorInicial = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade de Parcelas: ");
+            calcular.qtdParcelas = int.Parse(Console.ReadLine());
+
+            Console.Write(calcular.CalcularParcelas());
+
+            Console.ReadKey();
+
+        }
+        
+    }
+   
+}
